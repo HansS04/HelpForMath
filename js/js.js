de4fe1da52f08c9e61ba85fe3faa9068d7a0e87a
it1926 <janslivka55@seznam.cz>
2020-11-10 20:03:19 +0100

    Prvni formulaře

---
+++ js/js.js
@@ -0,0 +1,29 @@
+/*------------------CTVEREC----------------------*/
+let ctvereca = 0;
+let ctvereca2 = 0;
+/*Obsah*/
+function obsahct(a){
+  
+   
+    return a * a;
+}
+document.getElementById('ctverecobs').addEventListener('click',function(){
+    ctvereca = document.getElementById('ctvereca').value;
+     
+
+    document.getElementById('vysledek1').innerHTML = "<p>" + obsahct(ctvereca).toFixed(2) + " cm^2 </p>"
+    ctvereca = 0;
+});
+/*obvod*/
+function obvodct(a){
+  
+   
+    return a * 4;
+}
+document.getElementById('ctverecobv').addEventListener('click',function(){
+    ctvereca2 = document.getElementById('ctvereca2').value;
+  
+
+    document.getElementById('vysledek2').innerHTML = "<p>" + obvodct(ctvereca2).toFixed(2) + " cm </p>"
+    ctvereca2 = 0;
+});
